e0f50ffe0549622a965dd485ebe5a98e80d6dea5
ya-tommy <65650249+ya-tommy@users.noreply.github.com>
2021-04-11 03:36:53 -0400

    fix effects

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ var speed = 75;
 function typedEffect() {
     if (i < name.length) {
         document.getElementById("typed").innerHTML += name.charAt(i);
-        i++;
+        i++
         setTimeout(typedEffect, speed);
     }
 }
