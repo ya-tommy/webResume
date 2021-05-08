78fb35a90d13106a61ffc1fcc5de0c26eabad772
ya-tommy <65650249+ya-tommy@users.noreply.github.com>
2021-05-07 23:27:01 -0400

    blur fix

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ function toggleTheme() {
     var theme = document.getElementsByTagName('link')[0];
     if (theme.getAttribute('href') == 'style.css') { 
         theme.href = 'nightStyle.css';
-        if(/iPhone|iPad|iPod|BlackBerry|MSIE/i.test(navigator.userAgent)){
+        if(/iPhone|iPad|iPod|BlackBerry|Firefox|MSIE/i.test(navigator.userAgent)){
             document.getElementById("mymenu").style.backgroundColor = "rgb(48, 55, 61)";
         }
     } 
     else { 
         theme.href = 'style.css';
-        if(/iPhone|iPad|iPod|BlackBerry|MSIE/i.test(navigator.userAgent)){
+        if(/iPhone|iPad|iPod|BlackBerry|Firefox|MSIE/i.test(navigator.userAgent)){
             document.getElementById("mymenu").style.backgroundColor = "rgb(226, 234, 240)";
         }
     }
@@ -25,13 +25,13 @@ document.getElementById("cb").checked = checked;
 var theme = document.getElementsByTagName('link')[0];
 if (document.getElementById('cb').checked ==  true) {
     theme.href = 'nightStyle.css';
-    if(/iPhone|iPad|iPod|BlackBerry|MSIE/i.test(navigator.userAgent)){
+    if(/iPhone|iPad|iPod|BlackBerry|Firefox|MSIE/i.test(navigator.userAgent)){
         document.getElementById("mymenu").style.backgroundColor = "rgb(48, 55, 61)";
     }
 }
 else {
     theme.href = 'style.css';
-    if(/iPhone|iPad|iPod|BlackBerry|MSIE/i.test(navigator.userAgent)){
+    if(/iPhone|iPad|iPod|BlackBerry|Firefox|MSIE/i.test(navigator.userAgent)){
         document.getElementById("mymenu").style.backgroundColor = "rgb(226, 234, 240)";
     }
 }
